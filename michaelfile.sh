c84fe9005567442bda66904384b849a8c809a14a
mike <bleosas222@gmail.com>
2026-01-28 17:35:41 +0000

    my script

---
+++ michaelfile.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash
+
+
+# i want to print out some of my motivation
+
+year="within 6months"
+
+focus="No giving up"
+
+echo "i will make it big in devops"
+
+
+echo "i am learning everyday, this is my 6th weeks learning devops"
+
+echo "i will get my first devops job $year"
+
+echo "so $focus"
+
+
+
+#just a simple bash scrips 
