037a26d0ebcaca14e27b3401411288d7b69d312c
D-Best_64 <jhvug967@gmail.com>
2026-01-31 17:23:15 +0000

    D-Best_Script.sh

---
+++ D-Best_Script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Make the system talk back at you
+#
+#
+# Location = "UK"
+#
+#
+# echo "what is your name"
+#
+# read name
+#
+# echo "how old are you"
+#
+# read age
+#
+# "thank you $name for coming to the $location you are $age years old and we love you
+#
+# end of script
