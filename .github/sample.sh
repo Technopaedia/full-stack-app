9d470c720ffa61b370a2967c7f436e554dadb685
tegstech1 <otple03@gmail.com>
2026-02-01 18:18:53 +0000

    tegstech-bit new update

---
+++ .github/sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Simple welcome script
+#
+# echo "Tegstech Cloud Solution"
+#
+# echo "welcome you all to the CI/CD session"
+#
+# echo "2026 Technopaedia devops cohort"
+#
+# echo "thanking everyone who is created to to make this happen"
+#
+# echo "this class is already part of our story and we loving it"
+#
+# echo "see you guys when we all win and share our stories"
+#
+# echo "Cheers"
+#
+# End here
