df23e04ce4bc1b4885ab940d9eda8524b1ac8d23
best <jhvug967@gmail.com>
2026-02-12 16:18:42 +0000

    new edit

--- D-Best_Script.sh
+++ D-Best_Script.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
-
 # Make the system talk back at you
-#
-#
-# Location = "UK"
-#
-#
-# echo "what is your name"
-#
-# read name
-#
-# echo "how old are you"
-#
-# read age
-#
-# "thank you $name for coming to the $location you are $age years old and we love you
-#
+echo "what is your name"
+read name
+echo "how old are you"
+read age
+echo "where are you located"
+read location
+echo "thank you $name for coming to the $location you are $age years old and we love you. and also make sure you stay with your family as many wives leave their family in the $location"
 # end of script
